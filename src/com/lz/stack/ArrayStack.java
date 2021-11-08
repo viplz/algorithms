482cb5eba697852ee62c7393b298aea95f62d351
LzMaster <LzMaster@qq.com>
2021-11-09 00:23:48 +0800

    ...

---
+++ src/com/lz/stack/ArrayStack.java
@@ -0,0 +1,22 @@
+package com.lz.stack;
+
+/**
+ * @author lian-zhang
+ * @version 1.0
+ * @ClassName ArrayStack.java
+ * @Description TODO
+ * @createTime 2021年11月08日 22:25:32
+ */
+public class ArrayStack <T> {
+
+    private T[] stack;
+
+    public ArrayStack(T[] stack) {
+        this.stack = stack;
+    }
+
+    public void push(T ele){
+
+    }
+
+}
